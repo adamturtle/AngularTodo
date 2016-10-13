3d11a0428f6f6b16027eebb8f80486cb7cb1df6a
Adam Turtle <aturtle@benevity.com>
2016-10-13 14:34:27 -0600

    Added list filtering

--- js/tasksController.js
+++ js/tasksController.js
@@ -1,9 +1,15 @@
 app.controller('TasksController', function ($scope, tasksService) {
     $scope.tasks = tasksService
+    $scope.filter = ''
 
     $scope.setFilter = function (filter) {
         if (filter) {
-            return $scope.filter = filter
+            if (filter === 'completed') {
+            	return $scope.filter = {done: true}
+            }
+            else if (filter === 'remaining') {
+            	return $scope.filter = {done: false}
+            }
         }
         return $scope.filter = ''
     }
