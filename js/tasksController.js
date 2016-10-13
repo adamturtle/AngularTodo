d89921e075443f350134f68dda8fe7a4124dcaa4
Adam Turtle <aturtle@benevity.com>
2016-10-13 15:55:22 -0600

    Fixed filtering bug, added active state to filters, added "remove task" functionality

--- js/tasksController.js
+++ js/tasksController.js
@@ -1,14 +1,16 @@
 app.controller('TasksController', function ($scope, tasksService) {
     $scope.tasks = tasksService
     $scope.filter = ''
+    $scope.filterName = ''
 
     $scope.setFilter = function (filter) {
+    	$scope.filterName = filter            	            
         if (filter) {
-            if (filter === 'completed') {
-            	return $scope.filter = {done: true}
+        	if (filter === 'completed') {
+            	return $scope.filter = { done: true }
             }
             else if (filter === 'remaining') {
-            	return $scope.filter = {done: false}
+            	return $scope.filter = { done: false }
             }
         }
         return $scope.filter = ''
--- js/tasksService.js
+++ js/tasksService.js
@@ -1,13 +1,16 @@
 app.factory('tasksService', function () {
     return {
-        tasks: [
+        data: [
             { name: 'Task One', done: false },
             { name: 'Task Two', done: false },
             { name: 'Task Three', done: true },
             { name: 'Task Four', done: false }
         ],
+        removeTask: function (task) {
+            this.data.splice(task, 1)
+        },
         remaining: function () {
-            return this.tasks.reduce(function (count, task) {
+            return this.data.reduce(function (count, task) {
                 return task.done ? count : count + 1
             }, 0)
         }
